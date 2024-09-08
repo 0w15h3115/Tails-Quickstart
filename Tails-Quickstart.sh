eb2476d5d9f74e5bae65f58863cfb23d7c4d09ed
Owlshells <154110768+0w15h3115@users.noreply.github.com>
2024-09-08 20:38:25 +0000

    Update Tails-Quickstart.sh

--- Tails-Quickstart.sh
+++ Tails-Quickstart.sh
@@ -1,27 +1,30 @@
 #!/bin/bash
 
 # Update and upgrade system packages
-echo "Updating package lists..."
+echo "Updating package lists... This may take a while... "
 sudo apt update && sudo apt upgrade -y
+# Working
 
-# Install Git
-echo "Installing Git..."
-sudo apt install git -y
+# Install Pip
+sudo apt install python3-pip pipx -y
+#Working
 
 # Install Nmap (includes Ncat)
 echo "Installing Nmap..."
 sudo apt install nmap -y
+# Working
 
 # Install Apache server
 echo "Installing Apache server..."
 sudo apt install apache2 -y
 sudo systemctl start apache2
 sudo systemctl enable apache2
+# Working
 
 # Install Impacket via pip
 echo "Installing Impacket..."
-sudo apt install python3-pip -y
-pip3 install impacket
+apt install python3-impacket
+# Working
 
 # Install Build-Essential for compiling software
 echo "Installing build-essential..."
@@ -32,6 +35,7 @@ echo "Installing Sliver..."
 wget https://github.com/BishopFox/sliver/releases/latest/download/sliver-server_linux -O sliver-server
 chmod +x sliver-server
 sudo mv sliver-server /usr/local/bin/sliver-server
+# Not Working
 
 # Download and Install Burp Suite Community Edition
 echo "Installing Burp Suite..."
@@ -39,22 +43,9 @@ wget "https://portswigger.net/burp/releases/download?product=community&version=2
 chmod +x burpsuite.sh
 sudo ./burpsuite.sh
 
-# Install UPX from local file (assuming you manually transferred it)
-echo "Installing UPX from local file..."
-# Adjust the path if necessary
-tar -xf /media/usb/upx-3.96-amd64_linux.tar.xz
-sudo mv upx-3.96-amd64_linux/upx /usr/local/bin/
-
 # Install Netexec
-echo "Installing Netexec..."
-git clone https://github.com/mgrosse/netexec.git
-cd netexec
-make
-sudo mv netexec /usr/local/bin/netexec
-cd ..
-
-# Install ProxyChains
-echo "Installing ProxyChains..."
-sudo apt install proxychains4 -y
+sudo apt install pipx git
+pipx ensurepath
+pipx install git+https://github.com/Pennyw0rth/NetExec
 
 echo "All available tools installed! Apache server is running!"
